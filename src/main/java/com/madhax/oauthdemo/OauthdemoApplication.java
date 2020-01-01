2a7b9e94131bcb89c8583fd0786631adc53ba4e9
James Cathcart <cathcart.jm@gmail.com>
2019-12-31 21:37:44 -0700

    Removed old code, demo is functional

--- src/main/java/com/madhax/oauthdemo/OauthdemoApplication.java
+++ src/main/java/com/madhax/oauthdemo/OauthdemoApplication.java
@@ -4,13 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -18,29 +12,7 @@ public class OauthdemoApplication {
 
     private final static Logger log = LoggerFactory.getLogger(OauthdemoApplication.class);
 
-    @RequestMapping(value = {"/user"}, produces = "application/json")
-    public Map<String, Object> user(OAuth2Authentication user) {
-
-        log.debug("/user endpoint called.");
-        Map<String, Object> userInfo = new HashMap<>();
-
-        userInfo.put(
-                "user",
-                user.getUserAuthentication().getPrincipal()
-        );
-
-        userInfo.put(
-                "authorities",
-                AuthorityUtils.authorityListToSet(
-                        user.getUserAuthentication().getAuthorities()
-                )
-        );
-
-        return userInfo;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(OauthdemoApplication.class, args);
     }
-
 }
