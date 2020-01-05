1b5bc4de19ee1fe68ef2b27fd6d6cbe1224438b1
James Cathcart <cathcart.jm@gmail.com>
2020-01-05 11:48:37 -0700

    Hotfix - made the application stateless

--- src/main/java/com/madhax/oauthdemo/security/WebSecurityConfigurer.java
+++ src/main/java/com/madhax/oauthdemo/security/WebSecurityConfigurer.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -56,5 +58,12 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
                         passwordEncoder.encode(ADMIN_PASSWORD)
                 )
                 .roles(USER_ROLE, ADMIN_ROLE);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
