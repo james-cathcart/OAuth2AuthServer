c8715f947f85da342a0ae2068d7d4900d323d596
James Cathcart <cathcart.jm@gmail.com>
2020-01-16 08:22:46 -0700

    Creating skeleton test for AppController permissions tests

---
+++ src/test/java/com/madhax/oauthdemo/integ/AppControllerTest.java
@@ -0,0 +1,46 @@
+package com.madhax.oauthdemo.integ;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AppControllerTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+    }
+
+    @Test
+    void readCheck() {
+        fail();
+    }
+
+    @Test
+    void writeCheck() {
+        fail();
+    }
+
+    @Test
+    void deleteCheck() {
+        fail();
+    }
+}
