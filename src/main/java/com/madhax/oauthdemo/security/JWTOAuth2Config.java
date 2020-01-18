ac87c3689018ff4b82a2f899e0c9b16fbc8b9978
James Cathcart <cathcart.jm@gmail.com>
2020-01-18 08:20:49 -0700

    Refactored OAuth2 configuration so that the custom DefaultTokenServices is being used.

--- src/main/java/com/madhax/oauthdemo/security/JWTOAuth2Config.java
+++ src/main/java/com/madhax/oauthdemo/security/JWTOAuth2Config.java
@@ -1,9 +1,8 @@
 package com.madhax.oauthdemo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,28 +10,20 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
 public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
 
-    public static final String APP_USERNAME = "defaultApp";
-    public static final String APP_PASSWORD = "defaultAppPassword";
-
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authenticationManager;
     private UserDetailsService userDetailsService;
+    private DefaultTokenServices defaultTokenServices;
     private TokenStore tokenStore;
-    private JwtAccessTokenConverter jwtAccessTokenConverter;
-    private JWTTokenEnhancer jwtTokenEnhancer;
     private DataSource dataSource;
 
     @Autowired
@@ -40,62 +31,30 @@ public class JWTOAuth2Config extends AuthorizationServerConfigurerAdapter {
             PasswordEncoder passwordEncoder,
             AuthenticationManager authenticationManager,
             UserDetailsService userDetailsService,
+            @Qualifier("customTokenServices") DefaultTokenServices defaultTokenServices,
             TokenStore tokenStore,
-            JwtAccessTokenConverter jwtAccessTokenConverter,
-            JWTTokenEnhancer jwtTokenEnhancer,
             DataSource dataSource
     ) {
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.userDetailsService = userDetailsService;
+        this.defaultTokenServices = defaultTokenServices;
         this.tokenStore = tokenStore;
-        this.jwtAccessTokenConverter = jwtAccessTokenConverter;
-        this.jwtTokenEnhancer = jwtTokenEnhancer;
         this.dataSource = dataSource;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 
-        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-
-        tokenEnhancerChain
-                .setTokenEnhancers(
-                        Arrays.asList(
-                                jwtTokenEnhancer,
-                                jwtAccessTokenConverter
-                        )
-                );
-
         endpoints
                 .tokenStore(tokenStore)
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
-                .tokenServices(
-                        tokenServices(
-                                tokenEnhancerChain,
-                                endpoints.getClientDetailsService()
-                        )
-                );
+                .tokenServices(defaultTokenServices);
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource);
-    }
-
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices(TokenEnhancerChain tokenEnhancerChain, ClientDetailsService clientDetailsService) {
-
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setReuseRefreshToken(true);
-        defaultTokenServices.setAccessTokenValiditySeconds(300);
-        defaultTokenServices.setRefreshTokenValiditySeconds(600);
-        defaultTokenServices.setTokenStore(tokenStore);
-        defaultTokenServices.setTokenEnhancer(tokenEnhancerChain);
-        defaultTokenServices.setClientDetailsService(clientDetailsService);
-
-        return defaultTokenServices;
     }
 }
--- src/main/java/com/madhax/oauthdemo/security/JWTTokenStoreConfig.java
+++ src/main/java/com/madhax/oauthdemo/security/JWTTokenStoreConfig.java
@@ -1,14 +1,15 @@
 package com.madhax.oauthdemo.security;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 public class JWTTokenStoreConfig {
@@ -18,6 +19,21 @@ public class JWTTokenStoreConfig {
     @Bean
     public TokenStore tokenStore() {
         return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    @Qualifier("customTokenServices")
+    public DefaultTokenServices tokenServices() {
+
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setReuseRefreshToken(true);
+        defaultTokenServices.setSupportRefreshToken(true);
+        defaultTokenServices.setAccessTokenValiditySeconds(300);
+        defaultTokenServices.setRefreshTokenValiditySeconds(1800);
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setTokenEnhancer(jwtTokenEnhancerChain());
+
+        return defaultTokenServices;
     }
 
     @Bean
@@ -32,6 +48,20 @@ public class JWTTokenStoreConfig {
     @Bean
     public JWTTokenEnhancer jwtTokenEnhancer() {
         return new JWTTokenEnhancer();
+    }
+
+    @Bean
+    public TokenEnhancerChain jwtTokenEnhancerChain() {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+
+        tokenEnhancerChain.setTokenEnhancers(
+                Arrays.asList(
+                        jwtTokenEnhancer(),
+                        jwtAccessTokenConverter()
+                )
+        );
+
+        return tokenEnhancerChain;
     }
 
 }
