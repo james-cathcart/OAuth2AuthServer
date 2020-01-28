22c936ed52b796884152c11f7cdff16719116983
James Cathcart <cathcart.jm@gmail.com>
2020-01-28 06:38:29 -0700

    Application now reads properties from cloud config server. Note: when the cloud config server is deployed, the uri in bootstrap.yml will need to be changed

--- src/main/java/com/madhax/oauthdemo/bootstrap/Bootstrap.java
+++ src/main/java/com/madhax/oauthdemo/bootstrap/Bootstrap.java
@@ -2,6 +2,7 @@ package com.madhax.oauthdemo.bootstrap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +11,24 @@ public class Bootstrap implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
 
+    @Value("${token.access.timeout}")
+    private Integer accessTokenTimeout;
+
+    @Value("${token.refresh.timeout}")
+    private Integer refreshTokenTimeout;
+
     public Bootstrap() {
         log.debug("I am Bootstrap");
     }
 
+    private void listProperties() {
+        System.out.println("Access token timeout: " + accessTokenTimeout);
+        System.out.println("Refresh token timeout: " + refreshTokenTimeout);
+    }
+
     @Override
     public void run(String... args) throws Exception {
-
+        log.debug("Bootstrap is running!");
+        listProperties();
     }
 }
